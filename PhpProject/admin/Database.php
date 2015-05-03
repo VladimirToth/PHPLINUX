b7b8bda917b2753bf313c24d00aa79c63ee693cf
Vlado <Vlado@Vlado-PC>
2015-05-03 22:53:45 +0200

    Staffs1

---
+++ PhpProject/admin/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Db {
+    private $server = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "dentist";
+    private $link;
+
+    public function __construct() {
+        $this->link = mysqli_connect($this->server, $this->username, $this->password, $this->database) or die('Failed to connect.');
+    }
+
+    public function query($sql) {
+        return mysqli_query($this->link, $sql);
+    }
+
+    public function __destruct() {
+        mysqli_close($this->link);
+    }
+}
+?>
