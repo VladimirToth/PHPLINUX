62acc0b67d50a0ad1ee02ce36d9ba906dd9947b2
MinarcinyM <m.minarciny@gmail.com>
2015-05-05 19:49:26 +0200

    slider

--- PhpProject/index.php
+++ PhpProject/index.php
@@ -90,7 +90,7 @@
 </div><!--/.navbar -->
     </header>
 
-<script src="web/jssor/js/jquery-2.1.3.js"></script>
+<script src="web/js/jquery-2.1.3.js"></script>
 <script src="web/js/bootstrap.min.js"></script>
   <div class="container">
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -105,7 +105,7 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-        <img src="web/jssor/img/home/01.jpg" alt="First" width="460" height="345">
+        <img src="images/slider/01.jpg" alt="First" width="460" height="345">
         <div class="carousel-caption">
           <h3>First</h3>
           <p>This is the first ever picture in my carousel.</p>
@@ -113,7 +113,7 @@
       </div>
 
     <div class="item">
-        <img src="web/jssor/img/home/02.jpg" alt="Second" width="460" height="345">
+        <img src="images/slider/02.jpg" alt="Second" width="460" height="345">
         <div class="carousel-caption">
           <h3>Second</h3>
           <p>Hi, this si the second picture in carousel.</p>
@@ -121,7 +121,7 @@
       </div>
 
     <div class="item">
-        <img src="web/jssor/img/home/03.jpg" alt="Third" width="460" height="345">
+        <img src="images/slider/03.jpg" alt="Third" width="460" height="345">
         <div class="carousel-caption">
           <h3>Third</h3>
           <p>Hi, this si the third picture in carousel.</p>
@@ -129,7 +129,7 @@
       </div>
 
     <div class="item">
-        <img src="web/jssor/img/home/04.jpg" alt="Fourth" width="460" height="345">
+        <img src="images/slider/04.jpg" alt="Fourth" width="460" height="345">
         <div class="carousel-caption">
           <h3>Fourth</h3>
           <p>Hi, this si the fourth picture in carousel.</p>
--- PhpProject/libraries/views/head.php
+++ PhpProject/libraries/views/head.php
@@ -1,15 +1,22 @@
 
-<!--<title><?php echo $meta['title'] ?></title>-->
 <!--<link href="styles.css" rel="stylesheet" type="text/css"/>-->
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta charset="utf-8">
 <title>A dentist from another universe</title>
-<meta name="generator" content="Bootply" />
+<meta name="generator" content="Dentist" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="web/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <!--[if lt IE 9]>
 <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 <link href="web/css/styles.css" rel="stylesheet" type="text/css">
-<meta content="<?php echo $meta['description'] ?>" name="description"/>
-<meta content="<?php echo $meta['keyword'] ?>" name="keywords"/>
+
+<!--<meta content="<?php echo $meta['description'] ?>" name="description"/>
+<meta content="<?php echo $meta['keyword'] ?>" name="keywords"/>-->
+ <!--Bootstrap core JavaScript-->
+
+<script type="text/javascript" src="web/jssor/examples-bootstrap/bootstrap.min.js"></script>
+    
