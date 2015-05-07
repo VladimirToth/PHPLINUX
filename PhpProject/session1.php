9cf357f929103a9212b102f983473e2b59929a2e
Vlado <Vlado@Vlado-PC>
2015-05-07 19:33:34 +0200

    Change pictures directory

--- PhpProject/session1.php
+++ PhpProject/session1.php
@@ -1,16 +1,16 @@
-<?php
+<!--<?php
 // Start the session
 session_start();
-?>
+?>-->
 <!DOCTYPE html>
 <html>
 <body>
 
-<?php
+<!--<?php
 // Set session variables
 $_SESSION["favpage"] = "offer";
 echo "Session variables are set.";
-?>
+?>-->
 
 </body>
 </html>
